79c7e419d1fbb138b177edeccc4290ead46c4551
Jose Armando <Arellano.53j@gmail.com>
2025-08-21 18:35:02 -0400

    Enable SSL for production Postgres

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
 // db.js
 const { Pool } = require("pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const isProd = process.env.NODE_ENV === "production";
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool(
+  isProd
+    ? { connectionString, ssl: { rejectUnauthorized: false } } // needed on Render/most clouds
+    : { connectionString } // local dev
+);
 
 module.exports = { pool };
